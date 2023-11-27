370b5c6ec840e3f772eb6de641f8c37c03c7b0d7
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2023-11-27 23:08:40 +0000

    add new dyn array and more interesting scene

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <dyn-array.h>
+
 size_t elem_rc(const size_t row, const size_t col, const size_t cols) {
   return row * cols + col;
 }
@@ -60,23 +62,33 @@ bool rule_4(
       && alive_neighbors(row, col, rows, cols, board) == 3;
 }
 
-void update_board(const size_t rows, const size_t cols, bool in_out_board[]) {
-  bool* original_board = malloc(rows * cols);
-  memcpy(original_board, in_out_board, sizeof(bool) * rows * cols);
+void update_board(const size_t rows, const size_t cols, bool board[]) {
+  typedef struct change_t {
+    int offset_;
+    bool state_;
+  } change_t;
+  change_t* changes = NULL;
   for (size_t row = 0; row < rows; row++) {
     for (size_t col = 0; col < cols; col++) {
       if (
-        rule_1(row, col, rows, cols, original_board)
-        || rule_3(row, col, rows, cols, original_board)) {
-        in_out_board[elem_rc(row, col, cols)] = false;
+        rule_1(row, col, rows, cols, board)
+        || rule_3(row, col, rows, cols, board)) {
+        array_push(
+          changes,
+          ((change_t){.offset_ = elem_rc(row, col, cols), .state_ = false}));
       } else if (
-        rule_2(row, col, rows, cols, original_board)
-        || rule_4(row, col, rows, cols, original_board)) {
-        in_out_board[elem_rc(row, col, cols)] = true;
+        rule_2(row, col, rows, cols, board)
+        || rule_4(row, col, rows, cols, board)) {
+        array_push(
+          changes,
+          ((change_t){.offset_ = elem_rc(row, col, cols), .state_ = true}));
       }
     }
   }
-  free(original_board);
+  for (int i = 0, size = array_size(changes); i < size; i++) {
+    board[changes[i].offset_] = changes[i].state_;
+  }
+  array_free(changes);
 }
 
 void print_board(const size_t rows, const size_t cols, const bool board[]) {
@@ -93,11 +105,51 @@ int main(int argc, char** argv) {
   (void)argc;
   (void)argv;
 
-  enum { cols = 40 };
-  enum { rows = 20 };
+  enum { cols = 80 };
+  enum { rows = 35 };
 
   bool board[rows * cols];
   memset(board, 0, rows * cols);
+
+  board[elem_rc(5, 2, cols)] = true;
+  board[elem_rc(6, 2, cols)] = true;
+  board[elem_rc(5, 3, cols)] = true;
+  board[elem_rc(6, 3, cols)] = true;
+
+  board[elem_rc(5, 12, cols)] = true;
+  board[elem_rc(6, 12, cols)] = true;
+  board[elem_rc(7, 12, cols)] = true;
+  board[elem_rc(4, 13, cols)] = true;
+  board[elem_rc(8, 13, cols)] = true;
+  board[elem_rc(3, 14, cols)] = true;
+  board[elem_rc(9, 14, cols)] = true;
+  board[elem_rc(3, 15, cols)] = true;
+  board[elem_rc(9, 15, cols)] = true;
+  board[elem_rc(6, 16, cols)] = true;
+  board[elem_rc(4, 17, cols)] = true;
+  board[elem_rc(8, 17, cols)] = true;
+  board[elem_rc(5, 18, cols)] = true;
+  board[elem_rc(6, 18, cols)] = true;
+  board[elem_rc(7, 18, cols)] = true;
+  board[elem_rc(6, 19, cols)] = true;
+
+  board[elem_rc(3, 22, cols)] = true;
+  board[elem_rc(4, 22, cols)] = true;
+  board[elem_rc(5, 22, cols)] = true;
+  board[elem_rc(3, 23, cols)] = true;
+  board[elem_rc(4, 23, cols)] = true;
+  board[elem_rc(5, 23, cols)] = true;
+  board[elem_rc(2, 24, cols)] = true;
+  board[elem_rc(6, 24, cols)] = true;
+  board[elem_rc(1, 26, cols)] = true;
+  board[elem_rc(2, 26, cols)] = true;
+  board[elem_rc(6, 26, cols)] = true;
+  board[elem_rc(7, 26, cols)] = true;
+
+  board[elem_rc(3, 36, cols)] = true;
+  board[elem_rc(4, 36, cols)] = true;
+  board[elem_rc(3, 37, cols)] = true;
+  board[elem_rc(4, 37, cols)] = true;
 
   // small exploder
   // board[elem_rc(7, 10, cols)] = true;
@@ -123,11 +175,11 @@ int main(int argc, char** argv) {
   // board[elem_rc(12, 19, cols)] = true;
 
   // glider
-  board[elem_rc(10, 7, cols)] = true;
-  board[elem_rc(11, 8, cols)] = true;
-  board[elem_rc(11, 9, cols)] = true;
-  board[elem_rc(10, 9, cols)] = true;
-  board[elem_rc(9, 9, cols)] = true;
+  // board[elem_rc(10, 7, cols)] = true;
+  // board[elem_rc(11, 8, cols)] = true;
+  // board[elem_rc(11, 9, cols)] = true;
+  // board[elem_rc(10, 9, cols)] = true;
+  // board[elem_rc(9, 9, cols)] = true;
 
   for (;;) {
     printf("\33[H\33[2J\33[3J");
